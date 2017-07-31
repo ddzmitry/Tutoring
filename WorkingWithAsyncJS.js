7e28ec25f206912888c9462cee70fb9a40d2f73d
Dzmitry Dubarau <ddzmitry@yahoo.com>
2017-07-31 11:12:29 -0400

    async lesson

---
+++ WorkingWithAsyncJS.js
@@ -0,0 +1,92 @@
+
+var fetch = require('node-fetch');
+const chalk = require('chalk');
+const log = console.log;
+const fs = require('fs');
+
+let Kevin = {
+  name : 'Kevin',
+  age : 15,
+  job : "mechanic"
+}
+
+let html = `<h1>${Kevin.name} is ${Kevin.job} and x is ${Kevin.age} old</h1>`
+
+
+fs.writeFile("index.html", html, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
+
+
+
+
+async function myFirstAsyncFunction() {
+let response = await fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+let data = await response.json();
+return data
+}
+
+myFirstAsyncFunction()
+.then(data => console.log(data))
+.catch(reason => console.log(reason.message))
+
+
+
+// async function apicall() {
+
+//  const url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+//   fetch(url)
+//   .then((resp) => resp.json())
+//   .then(function(data) {
+//     let authors = data
+//     // console.log(authors)
+//     return await authors;
+//     // log(chalk.red(authors.data[0].url));
+//     //  log(chalk.blue(authors.data[0].url));
+//     // log(chalk.bgYellow(authors.data[0].url));
+//   })
+//   .catch(function(error) {
+//     // console.log(error);
+//   });   
+//   }
+
+// let status = apicall().then((data)=>{
+//   console.log(data)
+// })
+
+
+  let data = 
+  `──────▄▀▀▄────────────────▄▀▀▄────
+─────▐▒▒▒▒▌──────────────▌▒▒▒▒▌───
+─────▌▒▒▒▒▐─────────────▐▒▒▒▒▒▐───
+────▐▒▒▒▒▒▒▌─▄▄▄▀▀▀▀▄▄▄─▌▒▒▒▒▒▒▌──
+───▄▌▒▒▒▒▒▒▒▀▒▒▒▒▒▒▒▒▒▒▀▒▒▒▒▒▒▐───
+─▄▀▒▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌───
+▐▒▒▒▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐───
+▌▒▒▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌──
+▒▒▐▒▒▒▒▒▒▒▒▒▄▀▀▀▀▄▒▒▒▒▒▄▀▀▀▀▄▒▒▐──
+▒▒▌▒▒▒▒▒▒▒▒▐▌─▄▄─▐▌▒▒▒▐▌─▄▄─▐▌▒▒▌─
+▒▐▒▒▒▒▒▒▒▒▒▐▌▐█▄▌▐▌▒▒▒▐▌▐█▄▌▐▌▒▒▐─
+▒▌▒▒▒▒▒▒▒▒▒▐▌─▀▀─▐▌▒▒▒▐▌─▀▀─▐▌▒▒▒▌
+▒▌▒▒▒▒▒▒▒▒▒▒▀▄▄▄▄▀▒▒▒▒▒▀▄▄▄▄▀▒▒▒▒▐
+▒▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▐
+▒▌▒▒▒▒▒▒▒▒▒▒▒▒▀▒▀▒▒▒▀▒▒▒▀▒▀▒▒▒▒▒▒▐
+▒▌▒▒▒▒▒▒▒▒▒▒▒▒▒▀▒▒▒▄▀▄▒▒▒▀▒▒▒▒▒▒▒▐
+▒▐▒▒▒▒▒▒▒▒▒▒▀▄▒▒▒▄▀▒▒▒▀▄▒▒▒▄▀▒▒▒▒▐
+▒▓▌▒▒▒▒▒▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▐
+▒▓▓▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌
+▒▒▓▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌─
+▒▒▓▓▀▀▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐──
+▒▒▒▓▓▓▓▓▀▀▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▄▀▀▒▌─
+▒▒▒▒▒▓▓▓▓▓▓▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▒▒▒▒▒▐─
+`
+// console.log(chalk.yellow(data))
+
+let data2 = `
+
+
+`
