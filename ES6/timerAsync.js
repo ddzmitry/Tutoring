c4f0b195282691dfdddc51732020e44160fcfe36
Dzmitry Dubarau <ddzmitry@yahoo.com>
2017-12-05 20:11:18 -0500

    more ES 6 stuff

--- ES6/timerAsync.js
+++ ES6/timerAsync.js
@@ -46,3 +46,11 @@ const FetchAll = (urls = []) => Promise.all(
    FetchAll(['https://swapi.co/api/people/1','https://swapi.co/api/people/2'])
    .then(data=>console.log(data))
     
+
+    //  another good way of showing student how async works
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+export default (async function showResults(values) {
+  await sleep(500); // simulate server latency
+  window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
+});
